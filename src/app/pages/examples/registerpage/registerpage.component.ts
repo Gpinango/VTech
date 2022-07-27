8832b1e119c1eab3d1a50ae02d0e38e7b3be455a
Gpinango <guillermopinango3199@gmail.com>
2022-07-27 11:53:28 -0700

    push para demo

--- src/app/pages/examples/registerpage/registerpage.component.ts
+++ src/app/pages/examples/registerpage/registerpage.component.ts
@@ -1,87 +1,90 @@
-import { Component, OnInit, OnDestroy, HostListener } from "@angular/core";
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core'
 
 @Component({
-  selector: "app-registerpage",
-  templateUrl: "registerpage.component.html"
+  selector: 'app-registerpage',
+  templateUrl: 'registerpage.component.html',
 })
 export class RegisterpageComponent implements OnInit, OnDestroy {
-  isCollapsed = true;
-  focus;
-  focus1;
-  focus2;
-  constructor() {}
-  @HostListener("document:mousemove", ["$event"])
-  onMouseMove(e) {
-    var squares1 = document.getElementById("square1");
-    var squares2 = document.getElementById("square2");
-    var squares3 = document.getElementById("square3");
-    var squares4 = document.getElementById("square4");
-    var squares5 = document.getElementById("square5");
-    var squares6 = document.getElementById("square6");
-    var squares7 = document.getElementById("square7");
-    var squares8 = document.getElementById("square8");
+  isCollapsed = true
+  focus
+  focus1
+  focus2
+  focus3
+  focus4
 
-    var posX = e.clientX - window.innerWidth / 2;
-    var posY = e.clientY - window.innerWidth / 6;
+  constructor() {}
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e) {
+    var squares1 = document.getElementById('square1')
+    var squares2 = document.getElementById('square2')
+    var squares3 = document.getElementById('square3')
+    var squares4 = document.getElementById('square4')
+    var squares5 = document.getElementById('square5')
+    var squares6 = document.getElementById('square6')
+    var squares7 = document.getElementById('square7')
+    var squares8 = document.getElementById('square8')
+
+    var posX = e.clientX - window.innerWidth / 2
+    var posY = e.clientY - window.innerWidth / 6
 
     squares1.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.05 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.05 +
-      "deg)";
+      'deg)'
     squares2.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.05 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.05 +
-      "deg)";
+      'deg)'
     squares3.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.05 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.05 +
-      "deg)";
+      'deg)'
     squares4.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.05 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.05 +
-      "deg)";
+      'deg)'
     squares5.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.05 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.05 +
-      "deg)";
+      'deg)'
     squares6.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.05 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.05 +
-      "deg)";
+      'deg)'
     squares7.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.02 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.02 +
-      "deg)";
+      'deg)'
     squares8.style.transform =
-      "perspective(500px) rotateY(" +
+      'perspective(500px) rotateY(' +
       posX * 0.02 +
-      "deg) rotateX(" +
+      'deg) rotateX(' +
       posY * -0.02 +
-      "deg)";
+      'deg)'
   }
 
   ngOnInit() {
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.add("register-page");
+    var body = document.getElementsByTagName('body')[0]
+    body.classList.add('register-page')
 
-    this.onMouseMove(event);
+    this.onMouseMove(event)
   }
   ngOnDestroy() {
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.remove("register-page");
+    var body = document.getElementsByTagName('body')[0]
+    body.classList.remove('register-page')
   }
 }
