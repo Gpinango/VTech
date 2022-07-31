f1799da4f1f5d905d98a6777da79ad7b4767e824
Gpinango <guillermopinango3199@gmail.com>
2022-07-31 16:55:51 -0700

    Flexwave

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,30 @@
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HttpClientModule } from '@angular/common/http'
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar'
+import { TooltipModule } from 'ngx-bootstrap/tooltip'
+import { CollapseModule } from 'ngx-bootstrap/collapse'
+import { TabsModule } from 'ngx-bootstrap/tabs'
+import { PaginationModule } from 'ngx-bootstrap/pagination'
+import { AlertModule } from 'ngx-bootstrap/alert'
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker'
+import { CarouselModule } from 'ngx-bootstrap/carousel'
+import { ModalModule } from 'ngx-bootstrap/modal'
 
-import { BsDropdownModule } from "ngx-bootstrap/dropdown";
-import { ProgressbarModule } from "ngx-bootstrap/progressbar";
-import { TooltipModule } from "ngx-bootstrap/tooltip";
-import { CollapseModule } from "ngx-bootstrap/collapse";
-import { TabsModule } from "ngx-bootstrap/tabs";
-import { PaginationModule } from "ngx-bootstrap/pagination";
-import { AlertModule } from "ngx-bootstrap/alert";
-import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
-import { CarouselModule } from "ngx-bootstrap/carousel";
-import { ModalModule } from "ngx-bootstrap/modal";
+import { PagesModule } from './pages/pages.module'
 
-import { PagesModule } from "./pages/pages.module";
-
-import { IndexComponent } from "./pages/index/index.component";
-import { ProfilepageComponent } from "./pages/examples/profilepage/profilepage.component";
-import { RegisterpageComponent } from "./pages/examples/registerpage/registerpage.component";
-import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.component";
+import { IndexComponent } from './pages/index/index.component'
+import { ProfilepageComponent } from './pages/examples/profilepage/profilepage.component'
+import { RegisterpageComponent } from './pages/examples/registerpage/registerpage.component'
+import { LandingpageComponent } from './pages/examples/landingpage/landingpage.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
     // IndexComponent,
     // ProfilepageComponent,
     // RegisterpageComponent,
@@ -42,7 +41,7 @@ import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.c
     // TooltipModule.forRoot(),
     // CollapseModule.forRoot(),
     // TabsModule.forRoot(),
-    PagesModule
+    PagesModule,
     // PaginationModule.forRoot(),
     // AlertModule.forRoot(),
     // BsDatepickerModule.forRoot(),
@@ -50,6 +49,6 @@ import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.c
     // ModalModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
